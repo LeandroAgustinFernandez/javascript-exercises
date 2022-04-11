e00120c9bc9cd68f6c469a364ba507156fdbcda5
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2022-04-11 19:44:06 -0300

    04_removeFromArray Tests Complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
 const removeFromArray = function() {
-
+    let arrayValues = arguments[0];
+    let valuesToRemove = [...arguments].splice(1,[...arguments].length-1);
+    for (let i = 0; i < valuesToRemove.length; i++) {
+        arrayValues = arrayValues.filter(item => item !== valuesToRemove[i])
+    }
+    return arrayValues;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
