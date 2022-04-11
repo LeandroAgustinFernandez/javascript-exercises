029b19c0e9d18e00874488d77cc70a01a906cfb2
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2022-04-11 19:13:24 -0300

    02_repeatString Tests Complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string,repeatTimes) {
+    let resultString = '';
+    if (repeatTimes < 0) {
+        return 'ERROR';
+    }
+    if (string != '' || repeatTimes == 0) {
+        for (let i = 0; i < repeatTimes; i++) {
+            resultString += string;
+         }
+    }
+    return resultString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
