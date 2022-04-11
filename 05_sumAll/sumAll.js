be221946ae19a1a1267eb01a98a54870320c2143
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2022-04-11 19:57:49 -0300

    05_sumAll Tests Complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (number1, number2) {
+  if (number1 < 0 || number2 < 0) return "ERROR";
+  if (typeof number1 != "number" || typeof number2 != "number") return "ERROR";
+  let min = number1 < number2 ? number1 : number2;
+  let max = number1 > number2 ? number1 : number2;
+  let result = 0;
+  for (let i = min; i <= max; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
