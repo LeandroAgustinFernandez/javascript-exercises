383ee3904042fd79b40d26d728b33a7d9b259314
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2022-04-11 22:20:13 -0300

    07_tempConversion Tests Complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheitTemperature) {
+  let celciousTemperature = (fahrenheitTemperature-32)*(5/9);
+  return (Number.isInteger(celciousTemperature)) ?  Number(celciousTemperature) : Number(celciousTemperature.toFixed(1)); 
 };
 
-const ctof = function() {
-
+const ctof = function(celciousTemperature) {
+  let fahrenheitTemperature = ((celciousTemperature*(9/5))+32)
+  return (Number.isInteger(fahrenheitTemperature)) ?  Number(fahrenheitTemperature) : Number(fahrenheitTemperature.toFixed(1)); 
 };
 
 // Do not edit below this line
