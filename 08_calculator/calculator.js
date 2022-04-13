f7d9c7378f2e4d9a851d62b4a12233941468a4d4
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2022-04-13 16:51:19 -0300

    08_calculator Tests Complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,36 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arrayOfNumbers) {
+  return arrayOfNumbers.reduce((sum, value) => (sum += value), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arrayOfNumbers) {
+  return arrayOfNumbers.reduce((res, value) => (res *= value));
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let res = a;
+  for (let i = 1; i < b; i++) {
+    res = res * a;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a == 0) return 1;
+  let res = 1;
+  for (let index = a; index > 0; index--) {
+      res = res * index;
+  }
+  return res;
 };
-
+console.log(factorial(5));
 // Do not edit below this line
 module.exports = {
   add,
@@ -29,5 +38,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
