1f2cd5c9528802f3fa47358adc789c3d69a51080
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2022-04-13 18:12:11 -0300

    12_getTheOldest Tests Complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
 
+const findTheOldest = function (people) {
+  let oldest = people.sort((a, b) => {
+      
+    let first = ((a.yearOfDeath) ? a.yearOfDeath : new Date().getFullYear()) - a.yearOfBirth;
+    let second = ((b.yearOfDeath) ? b.yearOfDeath : new Date().getFullYear()) - b.yearOfBirth;
+    return first > second ? -1 : 1;
+  });
+  return oldest[0]
 };
 
 // Do not edit below this line
