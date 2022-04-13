53f3f833982b06bc7ea26bde7a09638f21f87ed1
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2022-04-13 17:21:39 -0300

    09_palindromes Tests Complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    let stringArray = string.toLowerCase().split('');
+    stringArray = stringArray.filter(letter => alphabet.includes(letter))
+    let filterString = stringArray.join('');
+    let reverString = stringArray.reverse().join('');
+    return filterString == reverString;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
